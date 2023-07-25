8ae230e71da61affe6b2c46d06b8e212312eb999
IgorMontezuma20 <igorminranda17@gmail.com>
2023-07-24 21:25:01 -0300

    feat: criando o endpoint de snacks

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,40 @@
-import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import { PrismaClient } from "@prisma/client"
+import dotenv from "dotenv"
+import express, { Express, Request, Response } from "express"
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT || 5000;
+const app: Express = express()
+const port = process.env.PORT || 5000
+const prisma = new PrismaClient()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req: Request, res: Response) => {
+  const { message } = req.body
+
+  if (!message) return res.status(400).send({ error: "Message is required" })
+
+  res.send({ message })
+})
+
+app.get("/snacks", async (req: Request, res: Response) => {
+  const { snack } = req.query
+
+  if (!snack) return res.status(400).send({ error: "Snack is required" })
+
+  // SELECT * FROM Snack WHERE snack = 'drink'
+  const snacks = await prisma.snack.findMany({
+    where: {
+      snack: {
+        equals: snack as string,
+      },
+    },
+  })
+
+  res.send(snacks)
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`)
 })
